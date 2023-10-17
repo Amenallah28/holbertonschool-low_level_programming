6b94223311584366ed0f7826c69057b55eb031a7
Amenallah28 <amenallahmrabet9@gmail.com>
2023-10-17 04:14:34 -0700

    i wish it works

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char alphabet;
+    char ALPHABET;
+
+    for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+    {
+        putchar(alphabet);
+
+    }
+    
+    for (ALPHABET = 'A'; ALPHABET <= 'Z' ; ALPHABET++)
+    {
+	putchar(ALPHABET);
+
+    }
+
+    putchar('\n');
+    return (0);
+}
+
