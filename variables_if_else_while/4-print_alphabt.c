02f5c6bd155a0bad5cdd0f8b47eb62ca7c57a12e
Amenallah28 <amenallahmrabet9@gmail.com>
2023-10-17 05:02:41 -0700

    work please

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char alphabet ;
+
+    for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+    {
+        if ( alphabet != 'e' && alphabet !='q')
+        putchar(alphabet);
+    }
+
+    putchar('\n');
+    return (0);
+}
