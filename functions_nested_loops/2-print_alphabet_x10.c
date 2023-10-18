9d448a06c412c3b8798b75ff5af57355fff3eb49
Amenallah28 <amenallahmrabet9@gmail.com>
2023-10-18 03:41:46 -0700

    10 x alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -5,7 +5,9 @@ void print_alphabet_x10(void);
 
     char alphabet = 'a';
     int i ;
-for (i=0 ;i<10 ;i++){
+    
+  for (i=0 ;i<10 ;i++)
+  {
     while (alphabet <= 'z')
    {
         _putchar(alphabet);
