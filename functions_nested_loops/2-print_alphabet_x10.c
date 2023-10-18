90d225905e3c3e202af4f8b57f51db3a65f2d729
Amenallah28 <amenallahmrabet9@gmail.com>
2023-10-18 04:52:39 -0700

    yeees

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,12 @@ void print_alphabet_x10(void)
     
   for (i=0 ;i<10 ;i++)
   {
-    while (alphabet <= 'z')
+    for (alphabet ='a' ; alphabet <= 'z'; alphabet++)
    {
         _putchar(alphabet);
 
-        alphabet++;
-	
     }
-         _putchar('\n');
+             _putchar('\n');
 }
         
 }
