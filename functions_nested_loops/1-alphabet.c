fa41a70decea6c38aa98919c25a4ca590d209392
Amenallah28 <amenallahmrabet9@gmail.com>
2023-10-18 02:22:39 -0700

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+int main(void)
+{
+
+    char alphabet = 'a';
+
+    while (alphabet <= 'z')
+   {
+        _putchar(alphabet);
+
+        alphabet++;
+    }
+
+    _putchar('\n');
+    return (0);
+}
