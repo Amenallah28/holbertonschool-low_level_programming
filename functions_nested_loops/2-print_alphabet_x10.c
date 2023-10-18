6c496f3835e6d9b6cb0decd1cee9d54dc61f7d00
Amenallah28 <amenallahmrabet9@gmail.com>
2023-10-18 03:32:43 -0700

    10 x alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+void print_alphabet_x10(void);
+{
+
+    char alphabet = 'a';
+    int i ;
+for (i=0 ;i<10 ;i++){
+    while (alphabet <= 'z')
+   {
+        _putchar(alphabet);
+
+        alphabet++;
+    }
+
+    _putchar('\n');
+}
+
+}
