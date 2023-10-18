5abdd0b6e438d0808ca0b553696588e208a8cf12
Amenallah28 <amenallahmrabet9@gmail.com>
2023-10-18 07:12:52 -0700

    m tired

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -4,14 +4,16 @@ int print_last_digit(int n)
 {
 int x;
 x = n % 10;
-while (x > 11)
+
+while (x >= 10)
 {
 x = x % 10;
 }
+
 if (x <= 0)
 {
 x = x * -1;
 }
-_putchar(n + '0');
-return(x);
+_putchar(x + '0');
+return (x);
 }
