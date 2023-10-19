58ee968d7faad22cc4e886de7439f7abe753bc73
Amenallah28 <amenallahmrabet9@gmail.com>
2023-10-19 04:35:44 -0700

    print 9888

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,12 +2,17 @@
 #include <stdio.h>
 
 void print_to_98(int n)
-{     
+{
     if (n <= 98)
     {
-    for (; n < 99 ; n++) 
+    for (; n < 99 ; n++)
     {
-     printf("%d, ", n);
+    if (n != 98){
+        printf("%d, ", n);
+    }
+    else{
+    printf("%d", n);
+    }
     }
     printf("\n");
     }
@@ -20,9 +25,13 @@ void print_to_98(int n)
     {
     for ( ; n > 98 ; n--)
     {
-	printf("%d, ", n);
+            if (n != 98){
+        printf("%d, ", n);
+    }
+    else{
+    printf("%d", n);
+    }
     }
     printf("\n");
     }
 }
-
