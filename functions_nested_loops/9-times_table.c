5e5e0f0b5662abae39ac3166c5cb71d451057afc
Amenallah28 <amenallahmrabet9@gmail.com>
2023-10-19 02:03:33 -0700

    please

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,7 +8,14 @@ for(i = 0; i < 10; i++)
    {
      for(j = 0; j < 10 ; j++)
      {
-	    printf("  %d,",i*j);
+	     if (i*j <10)
+	     {
+	    printf("  %d",i*j);
+	     }
+	     if (i*j > 9)
+	     {
+	    printf(" %d",i*j);
+	     }
       }
      printf("\n");
     }
