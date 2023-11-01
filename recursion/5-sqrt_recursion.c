dde72907564c1d86329fab33e05ec5544ee210fd
Amenallah28 <amenallahmrabet9@gmail.com>
2023-11-01 12:54:58 +0100

    function that returns the natural square root of a number.

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -12,10 +12,10 @@ int check(int n, int i)
 	if (i * i > n)
 		return (-1);
 	else
-		return(check(n, i + 1));
+		return (check(n, i + 1));
 }
 /**
- * int _sqrt_recursion - gives the square root of natural num
+ * _sqrt_recursion - gives the square root of natural num
  * @n: int
  * Return: int
  */
@@ -25,5 +25,5 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 	else
-		return (check(n, 2));
+		return (check(n, 1));
 }
