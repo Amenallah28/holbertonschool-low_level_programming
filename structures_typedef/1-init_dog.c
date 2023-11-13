a0654c8ade47736fc26fa4877bd452733c4b1d02
Amenallah28 <amenallahmrabet9@gmail.com>
2023-11-13 12:57:59 +0100

    a function that prints a struct dog

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include "dog.h"
+
 /**
- * init_dog - structure
+ * init_dog- a function that initialize a variable of type struct dog
+ * @d: structure
  * @name: name
  * @age: age
  * @owner: owner
  */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
