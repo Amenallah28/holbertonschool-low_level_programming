ad1bbe7d5cc8cb2096747b54a3f6b0fde1a3861e
Amenallah28 <amenallahmrabet9@gmail.com>
2023-11-13 12:42:08 +0100

    new type struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef AHLA_NES
+#define AHLA_NES
+/**
+ * struct dog - structure
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*AHLA_NES*/
