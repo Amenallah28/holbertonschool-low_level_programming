a1fc0286962b3aab82701f5f5a5c6d1291c0ce2b
Amenallah28 <amenallahmrabet9@gmail.com>
2023-11-14 11:36:35 +0100

    a function that creates a new dog.

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,47 +12,47 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *dog;
-    int len, size;
+	dog_t *dog;
+	int len, size;
 
-    dog = malloc(sizeof(dog_t));
-    if (dog == NULL)
-        return (NULL);
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
 
-    if (name != NULL)
-    {
-        len = strlen(name);
-        dog->name = malloc(sizeof(char) * (len + 1));
-        if (dog->name == NULL)
-        {
-            free(dog);
-            return (NULL);
-        }
-        strcpy(dog->name, name);
-    }
-    else
-    {
-        dog->name = NULL;
-    }
+	if (name != NULL)
+	{
+		len = strlen(name);
+		dog->name = malloc(sizeof(char) * (len + 1));
+		if (dog->name == NULL)
+		{
+			free(dog);
+			return (NULL);
+		}
+		strcpy(dog->name, name);
+	}
+	else
+	{
+		dog->name = NULL;
+	}
 
-    if (owner != NULL)
-    {
-        size = strlen(owner);
-        dog->owner = malloc(sizeof(char) * (size + 1));
-        if (dog->owner == NULL)
-        {
-            free(dog->name);
-            free(dog);
-            return (NULL);
-        }
-        strcpy(dog->owner, owner);
-    }
-    else
-    {
-        dog->owner = NULL;
-    }
+	if (owner != NULL)
+	{
+		size = strlen(owner);
+		dog->owner = malloc(sizeof(char) * (size + 1));
+		if (dog->owner == NULL)
+		{
+			free(dog->name);
+			free(dog);
+			return (NULL);
+		}
+		strcpy(dog->owner, owner);
+	}
+	else
+	{
+		dog->owner = NULL;
+	}
 
-    dog->age = age;
+	dog->age = age;
 
-    return (dog);
+	return (dog);
 }
