e19f799fb84e165d77256520f542c1d75dfcda7a
Amenallah28 <amenallahmrabet9@gmail.com>
2023-11-30 13:05:20 +0100

    freeing nodes

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,19 +1,24 @@
 #include "lists.h"
-
+/**
+ * add_node-a function that adds a new node at the end of a list_t list.
+ * @head:head of the list
+ * @str: the string
+ * Return:the address of the new element, or NULL if it failed
+*/
 list_t *add_node(list_t **head, const char *str)
 {
 
 	list_t *new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
-	free(new_node);
-	return (NULL);
+		free(new_node);
+		return (NULL);
 	}
 	new_node->str = strdup(str);
 
 	if (new_node->str == NULL)
 	{
-		free (new_node);
+		free(new_node);
 		return (NULL);
 	}
 	new_node->len = strlen(str);
