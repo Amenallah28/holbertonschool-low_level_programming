4b4caf53443fbc7af4a1acf4b46e26b444cca0a6
Amenallah28 <amenallahmrabet9@gmail.com>
2023-12-07 14:15:59 +0100

    a function that creates a file.

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,27 +1,32 @@
 #include "main.h"
-
+/**
+ * create_file-a function that reads a text file and prints it
+ * @filename:char
+ * @text_content:char;
+ * Return:1 or -1
+ */
 int create_file(const char *filename, char *text_content)
 {
-    int file, i;
+	int file, i;
 	char *array;
 	if (filename == NULL)
 	{
 		return (0);
 	}
-	file = open(filename, O_CREAT,0600);
+	file = open(filename, O_CREAT, 0600);
 	if (file == -1)
 	{
 		return (-1);
 	}
-    array =malloc(sizeof(char)*strlen(text_content));
+	array = malloc(sizeof(char) * strlen(text_content));
 
-    for (i=0;text_content[i] !='\0';i++)
-    {
-    array[i]=text_content[i];
-    write(file,array,strlen(text_content));
-    }
-    free(array);
-    close (file);	
-   
-    return(1);
+	for (i = 0; text_content[i] != '\0'; i++)
+	{
+		array[i] = text_content[i];
+		write(file, array, strlen(text_content));
+	}
+	free(array);
+	close(file);
+
+	return (1);
 }
