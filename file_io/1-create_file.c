f156d321355707ed8d47b1299a0dae6aff6fa498
Amenallah28 <amenallahmrabet9@gmail.com>
2023-12-07 14:06:58 +0100

    a function that creates a file.

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -2,7 +2,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    int file, file_w , i;
+    int file, i;
 	char *array;
 	if (filename == NULL)
 	{
@@ -19,10 +19,6 @@ int create_file(const char *filename, char *text_content)
     {
         array[i]=text_content[i];
     write(file,array,strlen(text_content));
-    }
-    if(file_w==-1)
-    {
-        return(-1);
     }
     close (file);	
    
