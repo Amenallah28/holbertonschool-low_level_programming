967209f450cca126319130ef63a16deaa3d2b584
Amenallah28 <amenallahmrabet9@gmail.com>
2023-12-08 14:06:15 +0100

    ahla nes

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,13 +13,13 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (0);
 	}
-	file = open(filename, O_CREAT, 0600);
+	file = open(filename, O_CREAT | O_WRONLY, 0600);
 	if (file == -1)
 	{
 		return (-1);
 	}
 	
-		write(file, text_content, strlen(text_content));
+	write(file, text_content, strlen(text_content));
 	close(file);
 
 	return (1);
