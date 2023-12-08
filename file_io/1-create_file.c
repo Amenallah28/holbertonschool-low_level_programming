95d56ab32c3a7ef2120740273c6414dba63ad8d9
Amenallah28 <amenallahmrabet9@gmail.com>
2023-12-08 14:02:59 +0100

    ahla nes

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,7 +8,6 @@
 int create_file(const char *filename, char *text_content)
 {
 	int file, i;
-	char *array;
 
 	if (filename == NULL)
 	{
@@ -19,14 +18,8 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	array = malloc(sizeof(char) * strlen(text_content));
-
-	for (i = 0; text_content[i] != '\0'; i++)
-	{
-		array[i] = text_content[i];
-		write(file, array, strlen(text_content));
-	}
-	free(array);
+	
+		write(file, text_content, strlen(text_content));
 	close(file);
 
 	return (1);
